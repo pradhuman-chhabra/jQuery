ee8bcea6531146fdc8bad5388b3f7737e3f36a15
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2022-12-17 23:46:55 +0530

    $("h1")is more versatile

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ $("h1").css("color","red");
 // this means that jquery will only execute 
 // $("h1").css("color","red");
 // when we have jQuery loaded in our system
+
+
+
+// NOTES
+
+// With a querySelector statement, you can select an element based on a CSS selector. This means you can select elements by ID, class, or any other type of selector. Using the getElementById method, you can only select an element by its ID. SO THE BEST ONE IS QUERY SELECTOR(ID/CLASSNAME/TAG)
+
+// using jQuery we can replace
+// document.querySelector('h1');
+
+// by simply typing
+// $('h1');
+
+// it even replaces document.querySelectorAll("button") as $("button") selects and returns all the buttons
