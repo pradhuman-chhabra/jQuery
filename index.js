a1eee683284f6199c625a21daef36f14765d0a70
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2022-12-17 17:06:43 +0530

    bssics

---
+++ index.js
@@ -0,0 +1,12 @@
+
+$("h1").css("color","red");
+
+// dollar sign is used to write  jquery
+// this dollar sign will target the h1 and the css of h1 and then it will change the color to red.
+
+
+// $(Document).ready(function({$("h1").css("color","red")});
+
+// this means that jquery will only execute 
+// $("h1").css("color","red");
+// when we have jQuery loaded in our system
